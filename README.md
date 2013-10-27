9e1d7b0e1ed83a8f16dc9daf2fef0bb9a792a73d
Stefano Michieletto <stefano.michieletto@dei.unipd.it>
2013-10-27 17:21:46 +0100

    Automatically quit Blender once meshes are exported for cmd line invokation (by Séverin Lemaignan) Switched visual meshes from STL to Collada (by Séverin Lemaignan) Added collision meshes created by the orginal Aldebaran NAO model

--- README.md
+++ README.md
@@ -3,8 +3,15 @@ nao_description
 
 URDF Model description of Aldebaran NAO
 
-Instructions (tested with Blender v2.66 - v2.69):
-- download this package
+Instructions - Automatic solution - (tested with Blender v2.66 - v2.69):
+- Download this package
+- Source the setup.sh of the workspace with this package
+- Download nao-v4.blend into the ./scripts/ directory
+- From the ./scripts/ directory, run blender nao-v4.blend -P io_export_separate.py 
+- Try the result using roslaunch nao_description rviz.launch
+
+Instructions - Manual solution - (tested with Blender v2.66 - v2.69):
+- Download this package
 - Download nao-v4.blend
 - Open the downloaded file with Blender after sourcing the setup.sh of the workspace with that package
 - Open a Text Editor window inside Blender (shit + F11)
--- script/io_export_separate.py
+++ script/io_export_separate.py
@@ -33,23 +33,31 @@
  #  POSSIBILITY OF SUCH DAMAGE.
  #
  # io_export_separate.py
- # Authors: Davide Zanin [davidezanin@gmail.com]
- #	    Roberto Bortoletto [roberto.bortoletto@dei.unipd.it]
- # 	    Stefano Michieletto [stefano.michieletto@dei.unipd.it]
+ # Authors: Vincent Rabaud [vincent.rabaud@gmail.com]
+ #          Séverin Lemaignan [severin.lemaignan@epfl.ch]
+ #	        Roberto Bortoletto [roberto.bortoletto@dei.unipd.it]
+ # 	        Stefano Michieletto [stefano.michieletto@dei.unipd.it]
  #
 
 # This script parses the nao-v4.blend file from Aldebaran robotics, parses it
-# into several .stl files to use with a URDF. It also decimates the meshes
-# to help with faster collision checking
+# into several .dae visual files and .stl collsion files to use with a URDF. 
+# It also decimates the collision meshes to help with faster collision checking
 
 import bpy
 import os
 import roslib.packages
 
-# Get the folder where the meshes will be saved
-mesh_dir = os.path.join(roslib.packages.get_pkg_dir('nao_description'), 'mesh', 'stl')
-if not os.path.isdir(mesh_dir):
-    os.makedirs(mesh_dir)
+# Get the folder where the visual meshes will be saved
+visual_mesh_dir = os.path.join(roslib.packages.get_pkg_dir('nao_description'), 'mesh', 'visual', 'dae')
+print("Exporting visual meshes to <%s>." % visual_mesh_dir)
+if not os.path.isdir(visual_mesh_dir):
+    os.makedirs(visual_mesh_dir)
+
+# Get the folder where the collision meshes will be saved
+collision_mesh_dir = os.path.join(roslib.packages.get_pkg_dir('nao_description'), 'mesh', 'collision', 'stl')
+print("Exporting collsion meshes to <%s>." % collision_mesh_dir)
+if not os.path.isdir(collision_mesh_dir):
+    os.makedirs(collision_mesh_dir)
 
 # Keep a copy of user selection
 bpy.ops.object.select_by_type(type="MESH")
@@ -68,6 +76,9 @@ for ob in bpy.data.objects:
     ob.hide = False
     ob.select = True
 
+    # Export single object to visual mesh (DAE)
+    bpy.context.scene.collada_export(filepath=os.path.join(visual_mesh_dir, ob.name + ".dae"), selected = True)
+
     # decimate the mesh
     bpy.context.scene.objects.active = ob
     bpy.ops.object.modifier_add(type='DECIMATE')
@@ -75,10 +86,10 @@ for ob in bpy.data.objects:
     # TODO: can an automatic parameter be chosen ? 0.5 seems to work.
     # It needs check with MoveIt! though to see if the modfied meshes create collisions
     # It could also only be done only for some parts (like Head/Torso)
-    mod.ratio = 1.0
+    mod.ratio = 0.5
 
-    # Export single object to STL
-    bpy.ops.export_mesh.stl(filepath=os.path.join(mesh_dir, ob.name + ".stl"))
+    # Export single object to collision mesh (STL)
+    bpy.ops.export_mesh.stl(filepath=os.path.join(collision_mesh_dir, ob.name + ".stl"))
 
     # remove the modifier to get back to normal once done
     bpy.ops.object.modifier_remove(modifier=mod.name)
@@ -88,3 +99,7 @@ bpy.ops.object.select_all(action="DESELECT")
 for ob in sel_obs:
     ob.select = True
 bpy.context.scene.objects.active = ob
+
+print("%s meshes exported." % len(sel_obs))
+
+bpy.ops.wm.quit_blender()
